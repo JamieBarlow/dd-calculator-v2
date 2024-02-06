379ba787a1ebb2b18e5790c8cccc4567e5b648d5
Jamie Barlow <jamiebarlow@gmail.com>
2024-02-06 21:39:43 +0000

    Add react-datepicker component and icon styling

--- react-vite-app/src/App.jsx
+++ react-vite-app/src/App.jsx
@@ -15,6 +15,18 @@ import {
   Footer,
   Step,
 } from "@cruk/cruk-react-components";
+
+// React datepicker with UK localisation
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import enGB from "date-fns/locale/en-GB";
+registerLocale("enGB", enGB);
+setDefaultLocale("enGB");
+import { RxCalendar } from "react-icons/rx";
+
+import { FaPlus } from "react-icons/fa";
+
 import styled from "styled-components";
 
 const InlineText = styled(Text)`
@@ -38,22 +50,46 @@ function App() {
         <Button>Refresh</Button>
       </Header>
       <Box margin="s">
-        <Heading>Direct Debit Processing Calendar</Heading>
+        <Heading h1 textAlign="center">
+          Direct Debit Processing Calendar
+        </Heading>
         <LeftBadge backgroundColor="tertiary" borderColor="primary">
           1
         </LeftBadge>
-        <Text textSize="l" margin="none" marginLeft="s">
-          Please enter any extra company holiday dates (e.g. Christmas office
-          closure dates) in the field below and click 'Add' to exclude them from
-          the list of potential working days. Make sure to include holidays for
-          both the current year <em>and</em> the previous year.
+        <Text textSize="l" margin="s">
+          Enter any extra company holiday dates which aren't UK bank holidays
+          (e.g. any extra Christmas office closure dates) from the year you are
+          generating results for, <b>and</b> the year previous to this.
         </Text>
-        <Text textSize="l">
-          <label htmlFor="company-dates">
-            Please enter in DD/MM/YYYY format and click 'Add' after each entry:
-          </label>
+        <Text textSize="l" margin="s">
+          Click 'Add' to exclude them from the list of potential working days.
         </Text>
-            
+        <Box margin="s" backgroundColor="primary">
+          <Text textSize="l" textColor="textOnPrimary">
+            <form id="extra-dates__form">
+              <label style={{ margin: 10 }} htmlFor="company-dates">
+                Please enter in DD/MM/YYYY format and click 'Add' after each
+                entry:
+              </label>
+              <DatePicker
+                placeholderText="    Click to select a date"
+                showIcon
+                id="company-dates"
+                pattern="([0-9]+(/[0-9]+)+)"
+                icon={<RxCalendar />}
+              />
+              <Button
+                id="addCompanyDatesBtn"
+                type="submit"
+                style={{ margin: 10 }}
+              >
+                <FaPlus />
+                Add
+              </Button>
+            </form>
+          </Text>
+        </Box>
+
         <Heading h3>H3</Heading>
         <Heading h4>H4</Heading>
         <Heading h5>H5</Heading>
@@ -62,6 +98,9 @@ function App() {
         <Text textSize="xxl">xxl</Text>
         <Text textSize="xl">xl</Text>
         <Text textSize="l">l</Text>
+      </Box>
+      <Box backgroundColor="primary">
+        <Text textColor="textOnPrimary">this is a box with colours</Text>
       </Box>
       <Button appearance="secondary">Button test</Button>
     </>
